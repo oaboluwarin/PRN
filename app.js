66410d23c6a0d14e721a5c4376ede8e227391e18
oaboluwarin <oreoluwa.aboluwarin@andela.com>
2018-07-05 10:18:58 +0100

    setup routes and basic controllers

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 import express, { json, urlencoded } from 'express';
 import morgan from 'morgan';
+import { userRouter, todoRouter } from './routes';
 
 const app = express();
 
 app.use(json());
 app.use(urlencoded({ extended: false }));
-app.use(morgan('tiny'));
+app.use(morgan('dev'));
 
-app.get('/', (req, res) => res.send({ message: 'Welcome! All raring to go.' }));
+app.use('/user', userRouter);
+app.use('/todo', todoRouter);
+
 
 export default app;
